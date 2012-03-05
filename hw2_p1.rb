3922585373fdd34af09d679aa2883f9366d756c3
jurera <jurerai@gmail.com>
2012-03-05 10:31:58 -0800

    še ne dela

--- hw2_p1.rb
+++ hw2_p1.rb
@@ -2,37 +2,35 @@
 
 class Numeric
  	
- 	@@singular_currency
+
 	@@currencies = {'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019, 'dollar' => 1}
 	def method_missing(method_id)
     
-		@@singular_currency = method_id.to_s.gsub( /s$/, '')
+		singular_currency = method_id.to_s.gsub( /s$/, '')
     
-		if @@currencies.has_key?(@@singular_currency)
-			self * @@currencies[@@singular_currency]
+		if @@currencies.has_key?(singular_currency)
+			self * @@currencies[singular_currency]
 		else
-			@@singular_currency = nil
+			
 			super
 		end
 	end
 	
 	
 	def in(currency)
-	
-		if @@singular_currency
-			print @@singular_currency
-			print exchange_currency = currency.to_s.gsub( /s$/, '')
-			print self
+
+		exchange_currency = currency.to_s.gsub( /s$/, '')
+		if @@currencies.has_key?(exchange_currency)			
+			self/@@currencies[exchange_currency]
 		else
 			super
 		end
-		@@singular_currency = nil
 
 	end
 end
+puts 5.rupees.in(:yen)
+puts 5.rupees.in(:yen).between?(7.2, 7.4)
+#~ puts 2.dollar.in(:yen)
 
-
-puts 2.yens.in(:euro)
-puts 2.dollar.in(:qwes)
 
 
